401b7b602c5a6fff8216d2111da7939374fbde22
saga-sanga <vulcan248@gmail.com>
2025-02-04 14:03:28 +0530

    feat: add count words to main

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = count_words(text)
+    print(f"{num_words} words found in the document")
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
